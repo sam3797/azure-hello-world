76f1259e72afe5bee08da963ec2cbc3a92d21d16
root <root@ip-172-31-86-198.ec2.internal>
2023-01-11 05:35:07 +0000

    testing phase

---
+++ eks/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# bg-deploy.sh <servicename> <version> <green-deployment.yaml>
+# Deployment name should be <service>-<version>
+
+DEPLOYMENTNAME=$1
+SERVICE=$2
+VERSION=$3
+#DEPLOYMENTFILE=$3
+
+#kubectl apply -f $DEPLOYMENTFILE
+kubectl apply -f deployment.yaml
+sleep 60;
+kubectl apply -f service.yaml
+sleep 120;
+kubectl apply -f deployment-1.yaml
+
+# Wait until the Deployment is ready by checking the MinimumReplicasAvailable condition.
+READY=$(kubectl get deploy $DEPLOYMENTNAME -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+while [[ "$READY" != "True" ]]; do
+    READY=$(kubectl get deploy $DEPLOYMENTNAME -o json | jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+    sleep 5
+done
+
+# Update the service selector with the new version
+#kubectl patch svc $SERVICE -p "{\"spec\":{\"selector\": {\"name\": \"${SERVICE}\", \"version\": \"${VERSION}\"}}}"
+kubectl patch svc $SERVICE -p "{\"spec\":{\"selector\": {\"app\": \"${VERSION}\"}}"
+
+echo "Done."
